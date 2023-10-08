f4e93bd15305edbf1c129390e302e90be02eca4d
Ivan <whitestranger7@gmail.com>
2023-10-08 15:22:42 +0200

    add ktor and room database support

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,10 +3,44 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val httpClient = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val response =
+            httpClient.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+                .body<MenuNetworkData>()
+        return response.menu
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database")
+            .allowMainThreadQueries().build()
+    }
+
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -14,6 +48,12 @@ class MainActivity : ComponentActivity() {
 
             LittleLemonTheme {
                 Navigation(navController = navController)
+            }
+        }
+
+        lifecycleScope.launch {
+            if(database.menuItemDao().isEmpty()) {
+                saveMenuToDatabase(fetchMenu())
             }
         }
     }
